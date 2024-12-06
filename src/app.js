b7a2b16e8f13a2d603edcb71cf2dbe1cdeff2a2f
Lisa Marie Moreno <108356315+lisamarie245@users.noreply.github.com>
2024-12-06 14:53:56 +0000

    RandomCards-Generator-Lisamarie245-FPS91

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let cardSimbols = ["♦", "♥", "♠", "♣"];
+  let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let randomSimbol =
+    cardSimbols[Math.floor(Math.random() * cardSimbols.length)];
+  let randomNumber = [Math.floor(Math.random() * cardNumbers.length)];
+
+  function redSimbols() {
+    const redColor =
+      randomSimbol == "♦" || randomSimbol == "♥" ? "red" : "black";
+
+    return redColor;
+  }
+
+  document.querySelector("#topSimbol").className = redSimbols() + " cardHeader";
+  document.querySelector("#topSimbol").textContent = randomSimbol;
+
+  //  Card Numbers
+  document.querySelector("#cardNumber").innerHTML = randomNumber;
+
+  //  Bottom Simbols
+  document.querySelector("#bottomSimbol").className =
+    redSimbols() + " cardFooter";
+  document.querySelector("#bottomSimbol").textContent = randomSimbol;
 };
